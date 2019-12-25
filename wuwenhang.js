528e9c76503b1b5397d5bf1d699bdc8c76ca51a9
wuwenhang <3128991660@qq.com>
2019-12-25 15:48:49 +0800

    wuwenhang

---
+++ wuwenhang.js
@@ -0,0 +1,127 @@
+	var pic2019_1 = document.getElementById("pic2019_1");
+	pic2019_1.onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDELy06RU07Re4FOTfhRxhM190205.shtml?spm=C88599.PiLNYLi7XooB.EIUYt31P78CN.1");
+	}
+
+	var pic2019_2A = document.getElementsByClassName("pic2019_2A");
+	pic2019_2A[0].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEfvH7IobmITNZ19adJIKb190205.shtml?spm=C88599.PiLNYLi7XooB.EIUYt31P78CN.3");
+	}
+	pic2019_2A[1].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEPAib0wXd522j6OEJYttg190205.shtml?spm=C88599.PiLNYLi7XooB.EIUYt31P78CN.9");
+	}
+	pic2019_2A[2].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEtE2xcgLxNj0HMjr7mQhq190205.shtml?spm=C88599.PiLNYLi7XooB.EIUYt31P78CN.19");
+	}
+	pic2019_2A[3].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEO3ynQWHO5SLA2GfoKRT1190205.shtml?spm=C88599.PiLNYLi7XooB.EIUYt31P78CN.35");
+	}
+
+	var pic2019_3A = document.getElementsByClassName("pic2019_3A");
+	pic2019_3A[0].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDE6YcNgOikDk3RXszcUKGN190205.shtml?spm=C88599.PScSoc4HNBPS.EUmw9YWOhIbK.87");
+	}
+	pic2019_3A[1].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDET9Y3hIFSkNr3sFVervMu190205.shtml?spm=C88599.PScSoc4HNBPS.EUmw9YWOhIbK.81");
+	}
+	pic2019_3A[2].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEzMv4vjXOgVpahDAilWCz190205.shtml?spm=C88599.PScSoc4HNBPS.EUmw9YWOhIbK.75");
+	}
+	pic2019_3A[3].onclick = function(){
+		window.open("http://tv.cctv.com/2019/02/05/VIDEcZ0pgzkVyZjBBtkDawf9190205.shtml?spm=C88599.PScSoc4HNBPS.EUmw9YWOhIbK.61");
+	}
+//以上为2019年春晚歌曲点播部分
+
+var erweima = document.getElementById("erweima");
+var qq = document.getElementById("qq");
+	//创建一个img元素节点
+	qq.onmouseover = function(){
+		var imgNode = document.createElement("img");
+		imgNode.src = "img/QQ码.jpg";
+		erweima.appendChild(imgNode);
+		imgNode.id = "s";
+		var s = document.getElementById('s');
+	}
+	qq.onmouseout = function(){
+		erweima.removeChild(s);
+	}
+//右侧固定窗口
+
+var pic2018_1 = document.getElementsByClassName("pic2018_1");
+pic2018_1[0].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEN6IAu0R4JdyzvJkFVzNG180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.5");
+}
+pic2018_1[1].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/16/VIDEZscsgjvvluK5JnCoNJE8180216.shtml?spm=C88599.PiLNYLi7XooB.S73829.25");
+}
+pic2018_1[2].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEycrpjThbOOGviHlqAT6Z180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.17");
+}
+pic2018_1[3].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEZVzavlJFyVuJTm2V8cXS180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.35");
+}
+pic2018_1[4].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEFCuJLRfdCxLLAvJGLlA8180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.3");
+}
+pic2018_1[5].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEB1pSvHwNtFuJe84Jz225180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.13");
+}
+pic2018_1[6].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/16/VIDEVg3IcdjAdASWCKZUAleo180216.shtml?spm=C88599.PiLNYLi7XooB.S73829.27");
+}
+pic2018_1[7].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDEk8OFe11QoMDsknJ35sjQ180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.9");
+}
+pic2018_1[8].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/16/VIDEvY1TY5gYrI9InLWNL6pO180216.shtml?spm=C88599.PiLNYLi7XooB.S73829.11");
+}
+pic2018_1[9].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDE0P0iTvmIwZyrf9c9676F180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.19");
+}
+pic2018_1[10].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/15/VIDE2QPfbLltNNLxPQ5gy4Xb180215.shtml?spm=C88599.PiLNYLi7XooB.S73829.7");
+}
+pic2018_1[11].onclick = function(){
+	window.open("http://tv.cctv.com/2018/02/16/VIDE5nZ4MhgIOFyH0w870jH3180216.shtml?spm=C88599.PiLNYLi7XooB.S73829.23");
+}
+//以上为2018年春晚歌曲点播部分
+
+var pic2017_1 = document.getElementsByClassName("pic2017_1");
+pic2017_1[0].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEXIyV0t31u1ZE50LM1PXm170128.shtml");
+}
+pic2017_1[1].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEBmhGVUi8kpIMqOTK4rlQ170128.shtml");
+}
+pic2017_1[2].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEWoMW3ofV1lT0NUZTal6t170128.shtml");
+}
+pic2017_1[3].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDERe3pumFML6Hyt3IpbZ2Z170128.shtml");
+}
+pic2017_1[4].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEusYSchzPj33wiImTWLUj170128.shtml");
+}
+pic2017_1[5].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDE8e6iSBTVuX5K8bktZ7jd170128.shtml");
+}
+pic2017_1[6].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDE2oWU1JfoJyqgAOiSAA2N170128.shtml");
+}
+pic2017_1[7].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEdwB6Q6SKjJhVRWwoPLDJ170128.shtml");
+}
+pic2017_1[8].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEni7x3s4Lx2nLsLuH3S3B170128.shtml");
+}
+pic2017_1[9].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDELTXjCh1mz7HowEFaD5ZH170128.shtml");
+}
+pic2017_1[10].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEaWNCQPZ74wAAsR0Jrcwe170128.shtml");
+}
+pic2017_1[11].onclick = function(){
+	window.open("http://chunwan.cctv.com/2017/01/28/VIDEE5lzsHGTzG203pxBu3c8170128.shtml");
+}
+//以上为2017年春晚歌曲点播部分
+
